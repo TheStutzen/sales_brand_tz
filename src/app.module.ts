36e6260d7a188c445f06d2b1800ebeb3149d232e
Max <max@Mac-mini-Max.dlink>
2024-07-02 04:26:07 +0700

    Refactoring get method

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { AmoCrmModule } from './amoCrm/amoCrm.module'
 
 @Module({
-  imports: [],
+  imports: [AmoCrmModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+
+  const config = new DocumentBuilder()
+    .setTitle('SalesBrandTZ(backend-API)')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('swagger', app, document)
+
+  await app.listen(3050)
 }
 bootstrap()
