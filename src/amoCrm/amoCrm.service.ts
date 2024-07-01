38a58ebc356ea721169ea9d1c9e5df66005bcdd9
Max <max@Mac-mini-Max.dlink>
2024-07-02 04:46:57 +0700

    Added e2e tests

--- src/amoCrm/amoCrm.service.ts
+++ src/amoCrm/amoCrm.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { Client } from 'amocrm-js'
 
 @Injectable()
 export class AmoCrmService {
   private client: Client
 
-  constructor() {
+  constructor(private configService: ConfigService) {
     this.client = new Client({
       domain: 'rootr',
       auth: {
-        client_id: process.env.CLIENT_ID,
-        client_secret: process.env.CLIENT_SECRET,
-        redirect_uri: process.env.REDIRECT_URI,
-        code: process.env.CODE,
-        bearer: process.env.BEARER,
+        client_id: this.configService.get<string>('CLIENT_ID'),
+        client_secret: this.configService.get<string>('CLIENT_SECRET'),
+        redirect_uri: this.configService.get<string>('REDIRECT_URI'),
+        code: this.configService.get<string>('CODE'),
+        bearer: this.configService.get<string>('BEARER'),
       },
     })
   }
